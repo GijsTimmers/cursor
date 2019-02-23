807569d2cf11bd3e37d8ef0c10ba2b3fcf67dc0e
GijsTimmers <timmers.gijs@gmail.com>
2019-02-23 20:47:36 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+  name = "cursor",
+  packages = ["cursor"],
+  version = "1.3.3",
+  description = "A small Python package to hide or show the terminal cursor",
+  author = "Gijs Timmers",
+  author_email = "timmers.gijs@gmail.com",
+  url = "https://github.com/GijsTimmers/cursor",
+  keywords = ["cursor", "terminal", "hide", "show"],
+  classifiers = [],
+  entry_points = {
+        "console_scripts": ["cursor_hide=cursor:hide",
+                            "cursor_show=cursor:show"]}
+)
